a444fbf1fc000144ad28ef0004cbce8b60f272c5
Henrique Becker <comopodebecker@gmail.com>
2025-01-26 15:49:22 +0000

    finalização do desafio aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -32,9 +32,15 @@ int main() {
         {0,0,0,0,3,0,3,0,0,0},
         {0,0,0,0,0,0,0,0,0,0},
         {3,3,3,3,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}
     };
 
+    for(int i=0;i<10 ;i++){
+        for(int j=0; j<10; j++){
+            printf("%d",tabuleiroGrande[i][j]);
+        };
+        printf("\n");
+    };
 
 
     // Nível Mestre - Habilidades Especiais com Matrizes
